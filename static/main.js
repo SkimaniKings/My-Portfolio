1249aba4e512db77874f108323fd239ccbf6a456
skimanikings <kimanisimon856@gmail.com>
2020-03-03 07:25:59 +0300

    added the navigatiuon bar and the icons

---
+++ static/main.js
@@ -0,0 +1,12 @@
+var typed = new Typed('.type', {
+    strings: [
+        'Front-End Developer',
+        'Back-End Developer',
+        'Entrepreneur',
+        
+    ],
+    typeSpeed:80,
+    backSpeed:80,
+    loop: true 
+  });
+
