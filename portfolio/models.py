631bfdc17d84dd3a07646ccd321d5fc6cb505214
skimanikings <kimanisimon856@gmail.com>
2020-03-05 17:51:03 +0300

    create a project model for posting of models

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,10 @@
-from django.db import models
 
+rom django.db import models
 # Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=80)
+    description = models.TextField()
+    link = models.URLField(max_length=250)
+    image = models.ImageField()
+    def __str__(self):
+        return self.name
